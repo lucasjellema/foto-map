ceea58cfd22b7a063e2fcf591b7116ec9e07311b
Lucas Jellema <lucasjellema@gmail.com>
2024-04-21 08:29:45 +0200

    support descroption in timeline editor

--- src/components/TimelineEditor.vue
+++ src/components/TimelineEditor.vue
@@ -5,6 +5,10 @@
 
             <v-card-text>
                 <v-text-field v-model="model.label" label="Label"></v-text-field>
+                <v-sheet class="flex-1-1-100  ma-0 pa-0 mb-5">
+                  <QuillEditor theme="snow" toolbar="essential" v-model:content="model.description" contentType="delta" />
+                </v-sheet>
+
                 <div class="mb-5">
                     <span>Preview:
                         <hr :style="{
@@ -37,6 +41,8 @@
     </v-container>
 </template>
 <script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 const model = defineModel(); // contains the timeline object 
 const emit = defineEmits(['saveTimeline', 'closeDialog']);
